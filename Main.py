6bd60bdeb0fc9111e1a5d0f48bc0d302e9837b19
Joshua-Zamora <jazamora6@miners.utep.edu>
2021-04-21 21:04:02 -0600

    Updated main method to read frames from video camera

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import os
-import time
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 
 def read_images(image_dir):
@@ -20,31 +20,26 @@ def select_matches_ransac(pts0, pts1):
     return pts0[choice], pts1[choice]
 
 
-def get_best_match(cards, frame):
+def get_best_match_info(cards, frame):
     orb = cv2.ORB_create()
     matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    keys = np.empty(len(cards), dtype=object)
-    descriptors = np.empty(len(cards), dtype=object)
-
-    for i in range(len(cards)):
-        keys[i], descriptors[i] = orb.detectAndCompute(cards[i], mask=None)
-    frame_keys, frame_desc = orb.detectAndCompute(frame, mask=None)
 
     most_matches = 0
     best_match = None
     coordinates = None
 
+    frame_keys, frame_desc = orb.detectAndCompute(frame, mask=None)
     frame_points = np.array([p.pt for p in frame_keys])
-    for i in range(len(cards)):
-        matches = matcher.match(frame_desc, descriptors[i])
 
-        dist = np.array([m.distance for m in matches])
+    for i in range(len(cards)):
+        keys, descriptor = orb.detectAndCompute(cards[i], mask=None)
+        matches = matcher.match(frame_desc, descriptor)
+
         ind1 = np.array([m.queryIdx for m in matches])
         ind2 = np.array([m.trainIdx for m in matches])
-        card_points = np.array([p.pt for p in keys[i]])
-        ds = np.argsort(dist)
+        card_points = np.array([p.pt for p in keys])
 
-        current_coordinates, _ = select_matches_ransac(frame_points[ind1[ds]], card_points[ind2[ds]])
+        current_coordinates, _ = select_matches_ransac(frame_points[ind1], card_points[ind2])
 
         if current_coordinates.shape[0] > most_matches:
             most_matches = current_coordinates.shape[0]
@@ -56,34 +51,24 @@ def get_best_match(cards, frame):
 
 def main():
     cards = np.load('card_images.npy', allow_pickle=True)
-    frames = np.load('frames.npy', allow_pickle=True)
 
-    start = time.time()
-    get_best_match(cards, frames[0])
-    end = time.time()
-    print(end - start)
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
 
-    fig, ax = plt.subplots(ncols=2)
-    ax[0].imshow(frames[0])
-    ax[1].imshow(get_best_match(cards, frames[0]))
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Video capture unresponsive")
+            break
 
-    fig, ax = plt.subplots(ncols=2)
-    ax[0].imshow(frames[1])
-    ax[1].imshow(get_best_match(cards, frames[1]))
+        cv2.imshow('frame', frame)
+        cv2.imshow('match', get_best_match_info(cards, frame))
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    fig, ax = plt.subplots(ncols=2)
-    ax[0].imshow(frames[2])
-    ax[1].imshow(get_best_match(cards, frames[2]))
-
-    fig, ax = plt.subplots(ncols=2)
-    ax[0].imshow(frames[3])
-    ax[1].imshow(get_best_match(cards, frames[3]))
-
-    fig, ax = plt.subplots(ncols=2)
-    ax[0].imshow(frames[4])
-    ax[1].imshow(get_best_match(cards, frames[4]))
-
-    plt.show()
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
